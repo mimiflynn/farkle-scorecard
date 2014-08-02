28eafe7b03364acd8dc1e6295be4e6b44f0d5b15
Mimi Flynn <mimiflynn@gmail.com>
2014-08-02 14:43:06 -0400

    Add new player popup

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,17 +3,42 @@ angular.module('farkle.controllers', [])
     .controller('RulesCtrl', function ($scope) {
     })
 
-    .controller('ScorecardCtrl', function ($scope, Players) {
+    .controller('ScorecardCtrl', function ($scope, $ionicPopup, Players) {
+        var newPlayerPopup = {
+            template: '<input type="text" ng-model="newPlayer.name">',
+            title: 'New Player',
+            subTitle: 'Name',
+            scope: $scope,
+            buttons: [
+                { text: 'Cancel' },
+                {
+                    text: '<b>Save</b>',
+                    type: 'button-positive',
+                    onTap: function(e) {
+                        if (!$scope.newPlayer) {
+                            console.log('need a name');
+                        } else {
+                            $scope.players.push($scope.newPlayer);
+                            console.log($scope.players);
+                        }
+                    }
+                }
+            ]
+        };
+
         $scope.players = Players.all();
 
         $scope.leftButtons = [
             {
-                type: 'button-icon icon ion-navicon',
-                tap: function (e) {
-                    console.log('Going back!');
-                }
+                type: 'button-icon icon ion-navicon'
             }
         ];
+
+        $scope.addNewPlayer = function(){
+            $ionicPopup.show(newPlayerPopup);
+        }
+
+        console.log($scope);
     })
 
     .controller('playerDetailCtrl', function ($scope, $stateParams, Players) {
--- www/js/services.js
+++ www/js/services.js
@@ -3,24 +3,33 @@ angular.module('farkle.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Players', function() {
-  // Might use a resource here that returns a JSON array
+    .factory('Players', function () {
+        // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var players = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
+        // Some fake testing data
+        var players = [
+            {
+                id: 0,
+                name: 'Mimi Flynn'
+            }, {
+                id: 1,
+                name: 'Anna Chapman'
+            }, {
+                id: 2,
+                name: 'Annette Haynes'
+            }, {
+                id: 3,
+                name: 'Zac'
+            }
+        ];
 
-  return {
-    all: function() {
-      return players;
-    },
-    get: function(playerId) {
-      // Simple index lookup
-      return players[playerId];
-    }
-  }
-});
+        return {
+            all: function () {
+                return players;
+            },
+            get: function (playerId) {
+                // Simple index lookup
+                return players[playerId];
+            }
+        }
+    });
